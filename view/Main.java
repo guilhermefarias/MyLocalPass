0aa41517a679c343952049822b0ce5c17a99b7f7
guilhermefarias <guilherme@guiky.com.br>
2012-06-09 01:36:03 -0300

    adicionando form na view main

--- view/Main.java
+++ view/Main.java
@@ -1,11 +1,25 @@
 package view;
 
+import java.util.Scanner;
+import entity.Account;
+
 public class Main {
+	public static final Scanner s = new Scanner("");
+	
 	public static void main(String[] args) {
+		
 		Main v = new Main();
 		v.viewAdd();
 	}
 	private void viewAdd(){
-		System.out.println("Nome da nome");
+		System.out.println("Digite o nome da conta:");
+		String name = s.next();
+		System.out.println("Digite o login da conta:");
+		String login = s.next();
+		System.out.println("Digite o password da conta:");
+		String password = s.next();
+		System.out.println("Digite o description conta:");
+		String description = s.next();
+		Account acc = new Account(name, login, password, description);
 	}
 }
