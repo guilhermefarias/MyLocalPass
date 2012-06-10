3d205abbaa220c28f67fce0e24a406bb31b6acb4
guilhermefarias <guilherme@guiky.com.br>
2012-06-09 23:58:33 -0300

    adicionado novos metodos

--- controller/ControllerAccount.java
+++ controller/ControllerAccount.java
@@ -1,5 +1,5 @@
 package controller;
 
-public class ControllerAccount {
 
+public class ControllerAccount {
 }
--- dataAcess/RepositoryAccountFile.java
+++ dataAcess/RepositoryAccountFile.java
@@ -1,8 +1,26 @@
 package dataAcess;
 
+import java.io.File;
+
 public class RepositoryAccountFile {
+	
+	public void findFile(String filename){
+		String directoryAcc = "." + File.separator + "senhas";
+		File directory = new File(directoryAcc);
+		String[] files = directory.list();
+	
+		if(files != null && files.length > 0){
+			for(int i=0; i < files.length; i++){
+				if(files[i].equalsIgnoreCase(filename)){
+					//arquivo encontrado;
+				}
+			}
+			//arquivo nao encontrado
+		}
+	}
+	
 	public void addAccount(){
-		
+
 	}
 	public void deleteAccount(){
 		
